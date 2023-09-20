b00170a5f9c03d761e8a4022856bcf4533f6d530
pipdaniels <deluxehay1@gmail.com>
2023-09-20 16:15:00 +0100

    This is the  first commit of the w3school project

---
+++ my_site/members/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.main, name='main'),
+    path('members/', views.members, name='Index'),
+    path('members/details/<int:id>', views.details, name='details'),
+    path('testing/', views.testing, name='testing'),
+]
---
+++ my_site/members/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
+# Create your views here.
+def members(request):
+    template=loader.get_template('all_members.html')
+    mymembers = Member.objects.all().values()
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context, request))
+
+def details(request, id):
+    template = loader.get_template('details.html')
+    mymember = Member.objects.all().get(id=id)
+    context = {
+        'mymember':mymember,
+    }
+    return HttpResponse(template.render(context, request))
+
+def main(request):
+    template=loader.get_template('main.html')
+    return HttpResponse(template.render())
+
+def testing(request):
+    template = loader.get_template('template.html')
+    context = {
+        'fruits':['Apple', 'Banana', 'Cherry'],
+    }
+    return HttpResponse(template.render(context, request))
+
